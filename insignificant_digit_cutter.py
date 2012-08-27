f12d35b08cc031ccc5b0bc908a811a780552c6dc
snakile <snakile@gmail.com>
2012-08-28 01:57:12 +0300

    renamed the iterable parameter to cut_insignificant_digits_lazy() from seq to iterable because it doesn't necessarily have to be a sequence.

--- insignificant_digit_cutter.py
+++ insignificant_digit_cutter.py
@@ -1,13 +1,13 @@
 def cut_insignificant_digits(number, places):
-    '''cut the least significant decimal digits of a number, leave only [places] decimal digits'''
+    '''cut the least significant decimal digits of a number, leaving only [places] decimal digits'''
     if  type(number) != float: return number
     number_as_str = str(number)
     end_of_number = number_as_str.find('.')+places+1
     if end_of_number > len(number_as_str): return number
     return float(number_as_str[:end_of_number])
 
-def cut_insignificant_digits_lazy(seq, places):
-    for obj in seq:
+def cut_insignificant_digits_lazy(iterable, places):
+    for obj in iterable:
         yield cut_insignificant_digits_recursively(obj, places)
 
 def cut_insignificant_digits_recursively(obj, places):

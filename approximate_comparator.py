14ac8ced699e0a10dbbd90b45e2c06755a756cb7
snakile <snakile@gmail.com>
2012-08-28 13:54:59 +0300

    improved is_almost_equal() documentation

--- approximate_comparator.py
+++ approximate_comparator.py
@@ -1,11 +1,12 @@
 from insignificant_digit_cutter import cut_insignificant_digits_recursively
 
 def is_almost_equal(first, second, places=7):
-    '''returns True if first and second equal. 
-    returns true if first and second aren't equal but have exactly the same
-    structure and values except for a bunch of floats which are just almost
-    equal (floats are almost equal if they're equal when we consider only the
-    [places] most significant digits of each.'''
+    '''returns False if first and second aren't equal up to a desired precision.
+    Given two data structures, returns True if all elements of these structures
+    (in any nesting level) are either equal or almost equal.
+    floats are almost equal if they're equal when we consider only the
+    [places] most significant digits of each.
+    '''
     if first == second: return True
     cut_first = cut_insignificant_digits_recursively(first, places)
     cut_second = cut_insignificant_digits_recursively(second, places)
